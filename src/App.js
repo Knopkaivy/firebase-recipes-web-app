6fdd9f96ab9f6713550cacc55e081c45b37eb2d8
Knopka <knopkaivy@gmail.com>
2022-08-27 14:17:52 -0500

    add firestore, new recipe form and logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import { useState } from 'react';
 import FirebaseAuthService from './FirebaseAuthService';
 import LoginForm from './components/LoginForm';
+import AddEditRecipeForm from './components/AddEditRecipeForm';
 
 import './App.css';
+import FirebaseFirestoreService from './FirebaseFirestoreService';
 
 function App() {
   const [user, setUser] = useState(null);
   FirebaseAuthService.subscribeToAuthChanges(setUser);
+
+  async function handleAddRecipe(newRecipe) {
+    console.log('from App component HAR');
+    try {
+      const response = await FirebaseFirestoreService.createDocument(
+        'recipes',
+        newRecipe
+      );
+      console.log('trying');
+      // TODO: fetch new recipes from firestore
+      alert(`successfully created recipe with an ID = ${response.id}`);
+    } catch (error) {
+      alert(error.message);
+    }
+  }
 
   return (
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
         <LoginForm existingUser={user} />
+      </div>
+      <div className="main">
+        <AddEditRecipeForm
+          handleAddRecipe={handleAddRecipe}
+        ></AddEditRecipeForm>
       </div>
     </div>
   );
--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -2,6 +2,8 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
+// import { getFirestore } from 'firebase/firestore';
+
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
