b729c87eeb49e1946871c6492b1ad642c4906df1
Knopka <knopkaivy@gmail.com>
2022-09-04 19:56:17 -0500

    update firebaseServiceAccount.json file name

--- functions/FirebaseConfig.js
+++ functions/FirebaseConfig.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const serviceAccount = require('./fir-recipes-7a9d7-firebase-adminsdk-4catn-3affa5599b.json');
+const serviceAccount = require('./firebaseServiceAccount.json');
 
 const FIREBASE_STORAGE_BUCKET = 'fir-recipes-7a9d7.appspot.com';
+
+const firebase_params = {
+  type: serviceAccount.type,
+  projectId: serviceAccount.project_id,
+  privateKeyId: serviceAccount.private_key_id,
+  privateKey: serviceAccount.private_key,
+  clientEmail: serviceAccount.client_email,
+  clientId: serviceAccount.client_id,
+  authUri: serviceAccount.auth_uri,
+  tokenUri: serviceAccount.token_uri,
+  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
+  clientX509CertUrl: serviceAccount.client_x509_cert_url,
+};
 
 const apiFirebaseOptions = {
   ...functions.config().firebase,
