4ae2402978ae313800b09624e73083b2797cc71e
Knopka <knopkaivy@gmail.com>
2022-09-08 08:07:59 -0500

    revert firebaseServiceAccount setup

--- functions/FirebaseConfig.js
+++ functions/FirebaseConfig.js
@@ -1,27 +1,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const serviceAccount = require(process.env
-  .REACT_APP_FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
-// const serviceAccount = require('./firebaseServiceAccount.json');
+const serviceAccount = require('./firebaseServiceAccount.json');
 
 const FIREBASE_STORAGE_BUCKET = 'fir-recipes-7a9d7.appspot.com';
-const firebase_params = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientX509CertUrl: serviceAccount.client_x509_cert_url,
-};
 
 const apiFirebaseOptions = {
   ...functions.config().firebase,
-  // credential: admin.credential.applicationDefault(),
-  credential: admin.credential.cert(firebase_params),
+  credential: admin.credential.applicationDefault(),
   projectId: 'fir-recipes-7a9d7',
 };
 admin.initializeApp(apiFirebaseOptions);
